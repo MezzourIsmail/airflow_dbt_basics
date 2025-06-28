bd7b0339fe5e4e23f6e40b91f3c374a5516de83a
mezzo <mezzour.ismail07@gmail.com>
2025-06-28 16:08:18 +0200

    set up first test

---
+++ dags/some_notions_pipeline.py
@@ -0,0 +1,28 @@
+from airflow.decorators import task, dag
+import datetime
+
+from airflow.sdk import PokeReturnValue
+
+
+@dag(
+    start_date=datetime.datetime(2021, 12, 1),
+    schedule='@daily',
+    catchup=False,
+)
+def task_dag_daily():
+    @task.sensor(poke_interval=60, timeout=300, mode="poke")
+    def check_odd_minutes() -> PokeReturnValue:
+        actual_minute = datetime.datetime.now().minute
+        print("actual_minute", actual_minute)
+        if actual_minute % 2 == 0:
+            return PokeReturnValue(is_done=True)
+        else:
+            return PokeReturnValue(is_done=False)
+
+    @task
+    def print_hello_ismail():
+        print("hello Ismail")
+
+    check_odd_minutes() >> print_hello_ismail()
+
+task_dag_daily()
