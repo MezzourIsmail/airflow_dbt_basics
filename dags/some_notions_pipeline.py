db061d61475f6c0deca198d078ef88e14b903c9a
mezzo <mezzour.ismail07@gmail.com>
2025-06-29 11:01:38 +0200

    publish first version of dag

--- dags/some_notions_pipeline.py
+++ dags/some_notions_pipeline.py
@@ -9,7 +9,7 @@ from airflow.sdk import PokeReturnValue
     schedule='@daily',
     catchup=False,
 )
-def task_dag_daily():
+def first_workflow():
     @task.sensor(poke_interval=60, timeout=300, mode="poke")
     def check_odd_minutes() -> PokeReturnValue:
         actual_minute = datetime.datetime.now().minute
@@ -23,6 +23,31 @@ def task_dag_daily():
     def print_hello_ismail():
         print("hello Ismail")
 
-    check_odd_minutes() >> print_hello_ismail()
 
-task_dag_daily()
+    @task
+    def check_pokemon_api_available():
+        import requests
+        response = requests.get("https://pokeapi.co/api/v2/pokemon/")
+        if response.status_code == 200:
+            print("check_pokemon_api_available")
+            data = response.json()
+            pokemon_names = [pokemon["name"] for pokemon in data["results"]]
+            print(pokemon_names)
+        else:
+            print("API IS NOT AVAILABLE")
+            raise Exception("API IS NOT AVAILABLE")
+        return pokemon_names
+
+    @task
+    def print_name(pokemon_name: str):
+        print(f"Pokémon name: {pokemon_name}")
+    """
+    .expand() is used in Airflow to run the same task multiple times
+    print_name.expand(pokemon_name=["pikachu", "charizard", "bulbasaur"])
+    is equivalent to:
+    for name in ["pikachu", "charizard", "bulbasaur"]:
+        print_name(pokemon_name=name)
+    """
+    check_odd_minutes() >> print_hello_ismail() >> print_name.expand(pokemon_name=check_pokemon_api_available())
+
+first_workflow()
