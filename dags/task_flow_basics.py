82a3620c7416ebe796a369b4db8edcbd4ec7e99c
mezzo <mezzour.ismail07@gmail.com>
2025-07-05 19:30:31 +0200

    add callbacks

--- dags/task_flow_basics.py
+++ dags/task_flow_basics.py
@@ -1,10 +1,18 @@
 from airflow.decorators import dag, task
 from datetime import datetime
+from airflow.exceptions import AirflowException, AirflowFailException
+
+
+def _handle_failed_dag_run(context):
+    print("DAG failed here the information about the failed run")
+    print(context)
+
 
 @dag(
     start_date=datetime(2024, 1, 1),
     schedule='@daily',
     catchup=False,
+    on_failure_callback=_handle_failed_dag_run,
 )
 def test_task_flow():
     @task
@@ -14,6 +22,7 @@ def test_task_flow():
     @task
     def test_alice():
         print("hello Alice")
+        raise AirflowFailException("Task failed for testing the callback")
 
     test_ismail() >> test_alice()
 
